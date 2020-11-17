c8c822e5c3cfaab72ed7ef2731ec0511a74e1507
Tornado3P9 <max.barzantny@yahoo.de>
2020-11-17 14:27:19 +0100

    updated time_spent

--- time_spent.py
+++ time_spent.py
@@ -14,3 +14,11 @@ total_time = end_time - start_time
 print("Time: ", total_time)
 
 # ('Time: ', 1.1205673217773438e-05)
+
+#####################################################################################
+# Jupyter Notebook
+import math
+f = lambda x, y: 3*x**2 + math.log(x**2 + y**2 + 1)
+f(17, 42) # 874.6275443904885
+
+%timeit f(17, 42) # 1.59 µs ± 46 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
