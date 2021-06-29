71326fe3171a4184f81db459ac5de6211f89c35a
user <max.barzantny@yahoo.de>
2021-06-29 20:00:28 +0200

    typo

--- floor_ceil_round.py
+++ floor_ceil_round.py
@@ -13,7 +13,7 @@ print(ceil(300.16))  # =>  301.0
 print(ceil(300.72))  # =>  301.0
 
 # round()
-print(round(51.6))           # =>   52
-print(round(80.23456, 2)     # =>   80.23
-print(round(100.000056, 3)   # =>  100.0
-print(round(-100.000056, 3)  # =>  100.0
+print(round(51.6))            # =>   52
+print(round(80.23456, 2))     # =>   80.23
+print(round(100.000056, 3))   # =>  100.0
+print(round(-100.000056, 3))  # =>  100.0
