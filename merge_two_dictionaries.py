0e8e8564dda8ca46198db4d8c35e32e03f546131
Tornado3P9 <max.barzantny@yahoo.de>
2023-06-17 12:46:15 +0200

    20230617

--- merge_two_dictionaries.py
+++ merge_two_dictionaries.py
@@ -1,4 +1,24 @@
-# The following method can be used to merge two dictionaries.
+## The following method can be used to merge two dictionaries.
+
+# from Python 3.9 and later versions
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'c': 3, 'd': 4}
+
+# Version 1
+merged_dict = dict1 | dict2
+
+print(merged_dict)   # {'a': 1, 'b': 2, 'c': 3, 'd': 4} dict1 and dict2 remain unchanged
+
+
+# Version 2
+dict1 |= dict2
+
+print(dict1)    # {'a': 1, 'b': 2, 'c': 3, 'd': 4} dict1 has been changed
+
+
+
+# before Python 3.9
 
 def merge_two_dicts(a, b):
     c = a.copy()   # make a copy of a 
@@ -12,13 +32,10 @@ print(merge_two_dicts(a, b)) # {'y': 3, 'x': 1, 'z': 4}
 
 
 
-
-
 ## In Python 3.5 and above, you can also do it like the following:
-#def merge_dictionaries(a, b):
-#   return {**a, **b}
-#
-#
-#a = { 'x': 1, 'y': 2}
-#b = { 'y': 3, 'z': 4}
-#print(merge_dictionaries(a, b)) # {'y': 3, 'x': 1, 'z': 4}
+def merge_dictionaries(a, b):
+   return {**a, **b}
+
+a = { 'x': 1, 'y': 2}
+b = { 'y': 3, 'z': 4}
+print(merge_dictionaries(a, b)) # {'y': 3, 'x': 1, 'z': 4}
